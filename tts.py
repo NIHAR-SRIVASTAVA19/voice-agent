739c4c116748f4e65a8cd77bd3841e50c6f629b3
NIHAR <niharsrivastava1904@gmail.com>
2026-08-22 15:14:54 +0530

    feat: add text-to-speech audio playback

---
+++ tts.py
@@ -0,0 +1,27 @@
+import os
+from sarvamai.play import play, save
+from dotenv import load_dotenv
+from sarvamai import SarvamAI
+
+
+load_dotenv()
+
+client = SarvamAI(
+    api_subscription_key=os.getenv("SARVAM_API_KEY")
+)
+
+def text_to_speech(
+    text: str,
+    language: str = "hi-IN",
+    speaker: str = "shubh"
+):
+    response = client.text_to_speech.convert(
+        model="bulbul:v3",
+        text=text,
+        language_code=language,
+        speaker=speaker,
+    )
+    save(response, "output.wav")
+    play(response)
+
+    return response
